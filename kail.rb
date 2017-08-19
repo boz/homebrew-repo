26540c649c30109fecb67f7ec5bf0d6cf39b33f4
goreleaserbot <goreleaser@carlosbecker.com>
2017-08-18 21:25:06 -0700

    kail version v0.3.5

--- kail.rb
+++ kail.rb
@@ -1,9 +1,9 @@
 class Kail < Formula
   desc "kubernetes tail - pod log viewer"
   homepage "https://github.com/boz/kail"
-  url "https://github.com/boz/kail/releases/download/v0.3.4/kail_0.3.4_darwin_amd64.tar.gz"
-  version "0.3.4"
-  sha256 "97fa84e1d8c4a6faaf63297f0f30317b92edf6c633d3b7e7df47aec7de07e038"
+  url "https://github.com/boz/kail/releases/download/v0.3.5/kail_0.3.5_darwin_amd64.tar.gz"
+  version "0.3.5"
+  sha256 "78d62e22489bc8e089c43c9be6d2bec343f2fde50ec8461ce0753c41659ff2e3"
 
   def install
     bin.install "kail"
