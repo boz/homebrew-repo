2bcbe3efc80e9fc00fa1fa7b9f7f86b5a6f8ca11
goreleaserbot <goreleaser@carlosbecker.com>
2019-05-16 15:55:25 -0700

    Brew formula update for kail version v0.9.1

--- kail.rb
+++ kail.rb
@@ -2,9 +2,9 @@
 class Kail < Formula
   desc "kubernetes tail - pod log viewer"
   homepage "https://github.com/boz/kail"
-  url "https://github.com/boz/kail/releases/download/v0.9.0/kail_0.9.0_darwin_amd64.tar.gz"
-  version "0.9.0"
-  sha256 "658b50a341018edd2bcef84d87c1fdd17bdfb8735b4351dced61e41513346e6b"
+  url "https://github.com/boz/kail/releases/download/v0.9.1/kail_0.9.1_darwin_amd64.tar.gz"
+  version "0.9.1"
+  sha256 "3824de171a73c773649401f2bdccbd138f0022333c69e39e4f194e8ebd08290b"
 
   def install
     bin.install "kail"
